73f8a2054bac75c81fff11bf6f9aa506772ab01b
JammingJack <amineseth1@gmail.com>
2021-06-12 16:23:26 +0100

    projections infos (date, duration and price) are now dispalayed next to film posture in each session(seance)

--- lib/salles-page.dart
+++ lib/salles-page.dart
@@ -28,31 +28,49 @@ class _SallesPageState extends State<SallesPage> {
                   itemCount: (listSalles == null) ? 0 : listSalles.length,
                   itemBuilder: (context, index) {
                     return Card(
-                        color: Colors.deepOrangeAccent,
+                        color: Colors.white70,
                         child: Column(
                           children: <Widget>[
                             Container(
                               width: double.infinity,
                               child: ElevatedButton(
                                 style: ElevatedButton.styleFrom(
-                                    primary: Colors.white),
+                                    primary: Colors.deepOrangeAccent),
                                 child: Text(this.listSalles[index]['name'],
-                                    style: TextStyle(color: Colors.black)),
+                                    style: TextStyle(color: Colors.white)),
                                 onPressed: () {
                                   loadProjection(listSalles[index]);
                                 },
                               ),
                             ),
-                            if(this.listSalles[index]['projections']!=null)
-                              Row(
-                                children: <Widget>[
-                                  Image.network(
-                              GlobalData.host+"/imageFilm/${this.listSalles[index]['currentProjection']['film']['id']}",
-                                    //key: ValueKey(new Random().nextInt(100)),
-                                    width: 100,),
-                                ],
+                            if (this.listSalles[index]['projections'] != null)
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Image.network(
+                                      GlobalData.host +
+                                          "/imageFilm/${this.listSalles[index]['currentProjection']['film']['id']}",
+                                      //key: ValueKey(new Random().nextInt(100)),
+                                      width: 150,
+                                    ),
+                                    Column(
+                                      children:<Widget> [
+                                        ...(this.listSalles[index]['projections'] as List<dynamic>).map((projection){
+                                          return ElevatedButton(
+                                              child: Text("${projection['seance']['heureDebut']} (${projection['film']['duree']}H) - ${projection['prix']}DH",),
+                                              style: ElevatedButton.styleFrom(
+                                                primary: Colors.deepOrangeAccent
+                                              ),
+                                              onPressed: (){},
+                                          );
+                                        })
+                                      ],
+                                    )
+                                  ],
+                                ),
                               )
-
                           ],
                         ));
                   })),
@@ -84,8 +102,9 @@ class _SallesPageState extends State<SallesPage> {
         .replaceAll("{?projection}", "?projection=filmNeededInfoProjection");
     http.get(Uri.parse(url)).then((resp) {
       setState(() {
-        salle['projections'] = json.decode(resp.body)['_embedded']['projections'];
-        salle['currentProjection']=salle['projections'][0];
+        salle['projections'] =
+            json.decode(resp.body)['_embedded']['projections'];
+        salle['currentProjection'] = salle['projections'][0];
       });
     }).catchError((err) {
       print(err);
